a0ec35359f4d08a7b70a105f9a6729e680a92f0d
mrganesh665 <mrganesh665>
2025-05-26 19:15:12 +0530

    paymenthistory collect payment fixed

--- src/features/PaymentHistory/PaymentHistoryFeature.tsx
+++ src/features/PaymentHistory/PaymentHistoryFeature.tsx
@@ -1,17 +1,18 @@
- import React from "react";
-import { View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
-import { getPaymentHistory } from "../../hooks/Payment";
-import { navigate } from "../../utils/navigationServices";
+import { useRouter } from "expo-router";
+import React from "react";
+import { View } from "react-native";
+import CustomPagination from "../../components/elements/CustomPagination/CustomPagination";
 import Loader from "../../components/elements/Loader/Loader";
 import PaymentHistory from "../../components/ui/PaymentHistory/PaymentHistory";
-import CustomPagination from "../../components/elements/CustomPagination/CustomPagination";
-import { FilterAppointmentFeature } from "../Appointment/FilterAppointmentFeature/FilterAppointmentFeature";
-import { transformData } from "../../utils/tools";
 import { pieColor1, pieColor2, pieColor3 } from "../../constants/COLORS";
+import { getPaymentHistory } from "../../hooks/Payment";
+import { transformData } from "../../utils/tools";
+import { FilterAppointmentFeature } from "../Appointment/FilterAppointmentFeature/FilterAppointmentFeature";
 
 const PaymentHistoryFeature = () => {
   const { data, setparams, loading, setPage, page } = getPaymentHistory();
+  const router = useRouter();
 
   const options = [
     {
@@ -36,20 +37,30 @@ const PaymentHistoryFeature = () => {
 
   // handle button of three dots
   const handleOptions = (options: any, item: any) => {
+   
     if (options.id == 2) {
-      navigate("viewinvoice", { paymentId: item?.appointmentId.paymentId });
-    } else if (options.id == 3) {
-      navigate("ViewAppointment", {
-        id: item?.appointmentId?._id,
-        selectedTab: 1,
+      router.push({
+        pathname: "/viewInvoice",
+        params: { paymentId:  item?.appointmentId.paymentId },
       });
+
+    } else if (options.id == 3) {
+
+      router.push({
+        pathname: "/viewAppointments",
+        params: { id: item?.appointmentId._id, selectedTab: 1},
+      });
+
+   
     } else if (options.id == 1) {
-      navigate("paymentpage", {
+     router.push({
+        pathname:"/paymentPage", 
+        params:{
         customerId: item?.customerId?.id || item?.customerId,
         appointmentId: item.appointmentId?.id,
         amount: item.payment?.amount,
-        appointmentData: transformData(item),
-      });
+        appointmentData: JSON.stringify(transformData(item)),
+      },});
     }
   };
 
