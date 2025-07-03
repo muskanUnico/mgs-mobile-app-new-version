bd48b2d8664473d0a6716c587e2068be139f552d
mrganesh665 <mrganesh665>
2025-07-03 19:37:54 +0530

    fixed policy editable

--- src/features/Cms/PrivacyPolicy.js
+++ src/features/Cms/PrivacyPolicy.js
@@ -1,36 +1,46 @@
 import React from "react";
-import { View } from "react-native";
+import {
+  TextInput,
+  View
+} from "react-native";
 
-import RenderHtml from "react-native-render-html";
-import { useWindowDimensions } from "react-native";
+import { cleanText } from "@/src/utils/tools";
 import { useTheme } from "../../context/ThemeContext";
 
-export const PrivacyPolicy = ({data}) => {
-  const { width } = useWindowDimensions();
+export const PrivacyPolicy = ({ value, onChangeText }) => {
+  // const { width } = useWindowDimensions();
   const { theme } = useTheme();
 
   return (
-    <View
-      style={{
-        marginHorizontal: 10,
-        paddingHorizontal: 10,
-        borderWidth: 1,
-        borderColor: "black",
-        backgroundColor: theme.brandGreyColor,
-        borderRadius:10,
-        fontFamily: "Regular"
 
-      }}
-    >
-      {/* {defaultData} */}
-
-      <RenderHtml
-        contentWidth={width}
-        style={{ fontFamily: "Regular"}}
-        source={{
-          html: data?.policy.privacyPolicy,
-        }}
-      />
-    </View>
+          <View
+            style={{
+              marginHorizontal: 10,
+              paddingHorizontal: 10,
+              borderWidth: 1,
+              borderColor: "black",
+              backgroundColor: theme.brandGreyColor,
+              borderRadius: 10,
+              fontFamily: "Regular",
+            }}
+          >
+            {/* {defaultData} */}
+            <TextInput
+              value={cleanText(value)}
+              onChangeText={onChangeText}
+              placeholder="Write privacy policy here..."
+              multiline
+              textAlignVertical="top"
+              scrollEnabled={true}
+              showsVerticalScrollIndicator={true}
+              style={{
+                minHeight: 100,
+                fontSize: 12,
+                padding: 10,
+                backgroundColor: "#fff",
+                borderRadius: 8,
+              }}
+            />
+          </View>
   );
 };
--- src/features/Cms/T&C.js
+++ src/features/Cms/T&C.js
@@ -1,32 +1,40 @@
+import { cleanText } from "@/src/utils/tools";
 import React from "react";
-import { View } from "react-native";
-
-import RenderHtml from "react-native-render-html";
-import { useWindowDimensions } from "react-native";
+import { TextInput, View } from "react-native";
 import { useTheme } from "../../context/ThemeContext";
 
-export const TermsAndConditions = ({ data }) => {
-  const { width } = useWindowDimensions();
+export const TermsAndConditions = ({ value, onChangeText }) => {
   const { theme } = useTheme();
+
+
 
   return (
     <View
       style={{
         marginHorizontal: 10,
         paddingHorizontal: 10,
+        paddingVertical: 10,
         borderWidth: 1,
         borderColor: "black",
         backgroundColor: theme.brandGreyColor,
         borderRadius: 10,
       }}
     >
-      {/* {defaultData} */}
+      <TextInput
+        value={cleanText(value)}
+        onChangeText={onChangeText}
+        placeholder="Write T&C here..."
+        multiline
+        textAlignVertical="top"
+        scrollEnabled={true}
+        showsVerticalScrollIndicator={true}
 
-      <RenderHtml
-        contentWidth={width}
-        style={{ fontFamily: "Regular" }}
-        source={{
-          html: data?.policy?.termsAndConditions,
+        style={{
+          minHeight: 200,
+          fontSize: 12,
+          padding: 10,
+          backgroundColor: "#fff",
+          borderRadius: 8,
         }}
       />
     </View>
