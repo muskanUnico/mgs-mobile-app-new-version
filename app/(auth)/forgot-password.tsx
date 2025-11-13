f6970b8df20ff0fa38766aaf43b1be2dfa260399
mrganesh665 <mrganesh665>
2025-11-13 21:13:30 +0530

    Forgot screen fixed

--- app/(auth)/forgot-password.tsx
+++ app/(auth)/forgot-password.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import AuthBg from "@/src/components/ui/AuthBg";
+import SetPasswordInfo from "@/src/components/ui/SetPassword/Info";
+import SetPassModule from "@/src/features/Auth/SetPassword";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const Forgot = () => {
-  return (
-    <View>
-      <Text>Forgot</Text>
-    </View>
-  )
+   return (
+    <AuthBg>
+      <View style={styles.header}>
+        <Text style={[styles.welcomeText, { color: "#43484e",    fontFamily: "BoldText"
+ }]}>
+          Forgot Password
+        </Text>
+      </View>
+      <SetPasswordInfo />
+      <SetPassModule />
+    </AuthBg>
+  );
 }
 
 export default Forgot
+
+
+const styles = StyleSheet.create({
+  header: {
+    alignItems: "center",
+  },
+
+  welcomeText: {
+    fontSize: 25,
+    alignItems: "center",
+    textAlign: "center",
+  },
+});
